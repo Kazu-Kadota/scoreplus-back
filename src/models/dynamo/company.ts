0e423e7bae92a6655e5ad5b6e28c5b2f36f86b61
Kazu-Kadota <kadota.kazuyuki@gmail.com>
2023-06-26 20:17:47 -0300

    feat(register): add system and analysis config to company info

--- src/models/dynamo/company.ts
+++ src/models/dynamo/company.ts
@@ -1,6 +1,33 @@
-export enum CompanyTypeEnum {
-  CLIENT = 'client',
-  FONT = 'font',
+import { Timestamp } from './timestamp'
+
+export enum CompanyPersonAnalysisConfigEnum {
+  MEMBER = 'member',
+  AGGREGATE = 'aggregate',
+  AUTONOMOUS = 'autonomous',
+  OPERATIONAL = 'operational',
+  ADMINISTRATIVE = 'administrative',
+}
+
+export enum CompanyPersonAnalysisConfigNumberEnum {
+  MEMBER = 182,
+  AGGREGATE = 60,
+  AUTONOMOUS = 1,
+  OPERATIONAL = 365,
+  ADMINISTRATIVE = 365,
+}
+
+export interface CompanyPersonAnalysisConfig {
+  member: number
+  aggregate: number
+  autonomous: number
+  operational: number
+  administrative: number
+}
+
+export interface CompanySystemConfig {
+  antt: boolean
+  biometry: boolean
+  serasa: boolean
 }
 
 export interface CompanyKey {
@@ -10,10 +37,8 @@ export interface CompanyKey {
 export interface CompanyBody {
   cnpj: string
   name: string
-  type: CompanyTypeEnum
+  analysis_config: CompanyPersonAnalysisConfig
+  system_config: CompanySystemConfig
 }
 
-export interface Company extends CompanyKey, CompanyBody {
-  created_at: string
-  updated_at: string
-}
+export interface Company extends CompanyKey, CompanyBody, Timestamp {}
