13bb88ab3fa3bcf103fbe99e545f301864d57d2e
Kazu-Kadota <kadota.kazuyuki@gmail.com>
2023-07-01 18:51:21 -0300

    fix(requestplus): set correctly general status

--- src/controllers/requestplus/send-request-analysis/person/status-constructor.ts
+++ src/controllers/requestplus/send-request-analysis/person/status-constructor.ts
@@ -1,6 +1,7 @@
 import { CompanySystemConfig, CompanySystemConfigEnum } from 'src/models/dynamo/company'
 import { RequestStatusEnum } from 'src/models/dynamo/request-enum'
 import { PersonAnalysisStatus } from 'src/models/dynamo/request-person'
+import removeEmpty from 'src/utils/remove-empty'
 
 const personStatusConstructor = (company_system_config: CompanySystemConfig): PersonAnalysisStatus => {
   const status = {} as PersonAnalysisStatus
@@ -13,7 +14,7 @@ const personStatusConstructor = (company_system_config: CompanySystemConfig): Pe
       : undefined
   }
 
-  if (Object.keys(status).length === 0) {
+  if (Object.keys(removeEmpty(status)).length === 0) {
     status.general = RequestStatusEnum.WAITING
   } else {
     status.general = RequestStatusEnum.PROCESSING
