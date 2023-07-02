aeac4862d2b2392adfe3e6b2c59c38b24617be2d
Kazu-Kadota <kadota.kazuyuki@gmail.com>
2023-07-01 21:45:39 -0300

    feat(lambda-handler): create namespace

--- src/models/lambda.ts
+++ src/models/lambda.ts
@@ -1,11 +1,26 @@
-import { APIGatewayProxyEventHeaders, APIGatewayProxyEventMultiValueHeaders } from 'aws-lambda'
+import { APIGatewayProxyEvent } from 'aws-lambda'
+import { UserInfoFromJwt } from 'src/utils/extract-jwt-lambda'
 
 export interface ReturnResponse<T> {
   body: T
 }
 
-export interface Response<T> extends ReturnResponse<T> {
-  headers?: APIGatewayProxyEventHeaders
-  multiValueHeaders?: APIGatewayProxyEventMultiValueHeaders
+export interface Response<T = any> extends ReturnResponse<T> {
+  headers?:
+  | {
+        [header: string]: boolean | number | string;
+    }
+  | undefined;
+  multiValueHeaders?:
+  | {
+        [header: string]: Array<boolean | number | string>;
+    }
+  | undefined;
   statusCode?: number
 }
+
+export interface Request extends APIGatewayProxyEvent {
+  user_info?: UserInfoFromJwt
+}
+
+export type Controller<T = any> = (req: Request) => Promise<Response<T>>
