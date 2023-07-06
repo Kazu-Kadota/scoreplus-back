2c06a872ef158759f046d131178668321f433839
Kazu-Kadota <kadota.kazuyuki@gmail.com>
2023-07-06 20:14:58 -0300

    fix(package): add sparticuz chromium and puppeteer core

--- src/utils/generete-pdf.ts
+++ src/utils/generete-pdf.ts
@@ -1,14 +1,21 @@
-import puppeteer from 'puppeteer'
+import chromium from '@sparticuz/chromium'
+import puppeteer from 'puppeteer-core'
 
 const generatePdf = async (template: string) => {
-  const browser = await puppeteer.launch({ headless: 'new' })
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(),
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  })
 
   const page = await browser.newPage()
 
   await page.setContent(template, { waitUntil: 'domcontentloaded' })
 
   const pdf_buffer = await page.pdf({
-    format: 'A4',
+    format: 'a4',
     printBackground: true,
   })
 
