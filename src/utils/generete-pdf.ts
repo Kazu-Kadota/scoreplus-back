bf52a772cc1d2673c0660214c63aa999787edd90
Kazu-Kadota <kadota.kazuyuki@gmail.com>
2023-07-07 02:27:58 -0300

    fix(requestplus/release-extract): try with playwright

--- src/utils/generete-pdf.ts
+++ src/utils/generete-pdf.ts
@@ -1,23 +1,29 @@
-import chromium from '@sparticuz/chromium'
-import puppeteer from 'puppeteer-core'
+// import chromium from '@sparticuz/chromium'
+import * as playwright from 'playwright-aws-lambda'
+// import puppeteer from 'puppeteer-core'
 
-import getStringEnv from './get-string-env'
+// import getStringEnv from './get-string-env'
 
-const STAGE = getStringEnv('STAGE')
+// const STAGE = getStringEnv('STAGE')
 
 const generatePdf = async (template: string) => {
-  const params = {
-    args: STAGE === 'local' ? puppeteer.defaultArgs() : chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    // Need to install chromium-browser on your pc: sudo apt-get install chromium-browser
-    executablePath: STAGE === 'local' ? '/usr/bin/chromium-browser' : await chromium.executablePath,
-    headless: STAGE === 'local' ? false : chromium.headless,
-    ignoreHTTPSErrors: true,
-  }
-  console.log(params)
-  const browser = await puppeteer.launch(params)
+  // const params = {
+  //   args: STAGE === 'local' ? puppeteer.defaultArgs() : chromium.args,
+  //   defaultViewport: chromium.defaultViewport,
+  //   // Need to install chromium-browser on your pc: sudo apt-get install chromium-browser
+  //   executablePath: STAGE === 'local' ? '/usr/bin/chromium-browser' : await chromium.executablePath,
+  //   headless: STAGE === 'local' ? false : chromium.headless,
+  //   ignoreHTTPSErrors: true,
+  // }
+  // console.log(params)
 
-  const page = await browser.newPage()
+  const browser = await playwright.launchChromium()
+
+  const context = await browser.newContext()
+
+  // const browser = await puppeteer.launch(params)
+
+  const page = await context.newPage()
 
   await page.setContent(template, { waitUntil: 'domcontentloaded' })
 
@@ -26,7 +32,7 @@ const generatePdf = async (template: string) => {
     printBackground: true,
   })
 
-  await browser.close()
+  // await browser.close()
 
   return pdf_buffer
 }
