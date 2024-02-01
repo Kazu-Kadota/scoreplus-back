add14b4006c56904dbfd30f2457101090df09474
Kazu-Kadota <kadota.kazuyuki@gmail.com>
2024-02-01 19:34:32 -0300

    feat(requestplus): release extract vehicle v2

--- src/controllers/requestplus/release-extract/vehicle/format-vehicle-analysis.ts
+++ src/controllers/requestplus/release-extract/vehicle/format-vehicle-analysis.ts
@@ -1,34 +1,36 @@
 import dayjs from 'dayjs'
-import { companyAnalysisConfigStrings } from 'src/constants/company'
-import { AnalysisResultEnum } from 'src/models/dynamo/answer'
-import { Company, CompanyVehicleAnalysisConfigEnum } from 'src/models/dynamo/company'
-import { VehicleRequest } from 'src/models/dynamo/request-vehicle'
 
-export interface PdfVehicleRequest extends VehicleRequest {
-  validity: string;
-  analysis_config_string?: string;
+import { companyAnalysisConfigStrings } from '~/constants/company'
+import { CompanyVehicleAnalysisConfigEnum } from '~/models/dynamo/enums/company'
+import { AnalysisResultEnum } from '~/models/dynamo/enums/request'
+import { RequestplusFinishedAnalysisVehicle } from '~/models/dynamo/requestplus/finished-analysis-vehicle/table'
+import { UserplusCompany } from '~/models/dynamo/userplus/company'
+
+export type PdfVehicleRequest = RequestplusFinishedAnalysisVehicle & {
+  validity: string
+  analysis_config_string?: string
 }
 
-const formatVehicleAnalysis = (analysis: VehicleRequest, company: Company) => {
+const formatVehicleAnalysis = (analysis: RequestplusFinishedAnalysisVehicle, company: UserplusCompany) => {
   let validity: string
 
-  if (analysis.vehicle_analysis_config.type === CompanyVehicleAnalysisConfigEnum.AUTONOMOUS) {
+  if (analysis.vehicle_analysis_type.type === CompanyVehicleAnalysisConfigEnum.AUTONOMOUS) {
     validity = 'Próximo embarque'
   } else {
     const finished_at = dayjs(analysis.finished_at).toDate()
-    finished_at.setDate(finished_at.getDate() + company.analysis_config[analysis.vehicle_analysis_config.type])
+    finished_at.setDate(finished_at.getDate() + company.analysis_config[analysis.vehicle_analysis_type.type])
 
     validity = dayjs(finished_at.toISOString()).format('DD/MM/YYYY')
   }
 
-  if (analysis.analysis_result !== AnalysisResultEnum.APPROVED) {
+  if (analysis.result !== AnalysisResultEnum.APPROVED) {
     validity = 'Inadequado para embarque'
   }
 
   const vehicle_analysis: PdfVehicleRequest = { ...analysis, validity }
 
-  if (vehicle_analysis.vehicle_analysis_config?.type) {
-    vehicle_analysis.analysis_config_string = companyAnalysisConfigStrings[analysis.vehicle_analysis_config.type]
+  if (vehicle_analysis.vehicle_analysis_type?.type) {
+    vehicle_analysis.analysis_config_string = companyAnalysisConfigStrings[analysis.vehicle_analysis_type.type]
   }
 
   return vehicle_analysis
