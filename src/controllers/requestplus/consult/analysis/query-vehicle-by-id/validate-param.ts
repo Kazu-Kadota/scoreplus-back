6ac3acd4446237d2fbfca114129f95723c727b22
Kazu-Kadota <kadota.kazuyuki@gmail.com>
2024-02-01 19:32:33 -0300

    feat(requestplus): query vehicle by id v2

---
+++ src/controllers/requestplus/consult/analysis/query-vehicle-by-id/validate-param.ts
@@ -0,0 +1,35 @@
+import Joi from 'joi'
+
+import BadRequestError from '~/utils/errors/400-bad-request'
+import logger from '~/utils/logger'
+
+export type RequestVehicleById = {
+  vehicle_id: string
+}
+
+const schema = Joi.object({
+  vehicle_id: Joi
+    .string()
+    .uuid()
+    .required(),
+}).required()
+
+const validateParam = (
+  data: Partial<RequestVehicleById | undefined>,
+): RequestVehicleById => {
+  const { value, error } = schema.validate(data, {
+    abortEarly: true,
+  })
+
+  if (error) {
+    logger.error({
+      message: 'Error on validate "query vehicle by id" path parameter',
+    })
+
+    throw new BadRequestError('Erro na validação do path parameter para obter veículo pelo id', error.stack as string)
+  }
+
+  return value
+}
+
+export default validateParam
