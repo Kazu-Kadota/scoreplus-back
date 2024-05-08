89cbba71c5aa5961a831980298d48494ae0aa208
Kazu-Kadota <kadota.kazuyuki@gmail.com>
2024-05-08 18:09:57 -0300

    feat(service): add s3 services

---
+++ src/services/aws/s3/put.ts
@@ -0,0 +1,38 @@
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { StreamingBlobPayloadInputTypes } from '@smithy/types'
+
+import logger from '~/utils/logger'
+
+export type PutS3Params<T = Record<string, string>> = {
+  body: StreamingBlobPayloadInputTypes
+  bucket: string
+  key: string
+  metadata?: T
+  s3Client: S3Client
+}
+
+export type PutS3Function<T = Record<string, string>> = (params: PutS3Params<T>) => Promise<void>
+
+const putS3: PutS3Function = async ({
+  body,
+  bucket,
+  key,
+  metadata,
+  s3Client,
+}) => {
+  logger.debug({
+    message: 'S3: PutObject',
+    bucket,
+  })
+
+  const command = new PutObjectCommand({
+    Bucket: bucket,
+    Key: key,
+    Body: body,
+    Metadata: metadata,
+  })
+
+  await s3Client.send(command)
+}
+
+export default putS3
